d0e2182d6b5c9970cbb1aa90238ae2117403e839
yumkim <yumkim@ethz.ch>
2024-12-04 14:37:48 +0100

    logs

---
+++ generated_cmds/input_permuted_mnist_stats/create_scripts.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+for f in *.txt
+do
+echo "#!/bin/bash" > ${f%.*}.sh
+echo -e "#SBATCH --signal=USR1@90" >> ${f%.*}.sh
+echo -e "#SBATCH --job-name="${f%.*}"\t\t\t# single job name for the array" >> ${f%.*}.sh
+echo -e "#SBATCH --mem=2G\t\t\t# maximum memory 100M per job" >> ${f%.*}.sh
+echo -e "#SBATCH --time=01:00:00\t\t\t# maximum wall time per job in d-hh:mm or hh:mm:ss" >> ${f%.*}.sh
+echo "#SBATCH --array=1-240" >> ${f%.*}.sh
+echo -e "#SBATCH --account=def-ashique" >> ${f%.*}.sh
+
+echo "cd "../../"" >> ${f%.*}.sh
+echo "FILE=\"\$SCRATCH/upgd/generated_cmds/input_permuted_mnist_stats/${f%.*}.txt\""  >> ${f%.*}.sh
+echo "SCRIPT=\$(sed -n \"\${SLURM_ARRAY_TASK_ID}p\" \$FILE)"  >> ${f%.*}.sh
+echo "module load python/3.7.9" >> ${f%.*}.sh
+echo "source \$SCRATCH/upgd/.upgd/bin/activate" >> ${f%.*}.sh
+echo "srun \$SCRIPT" >> ${f%.*}.sh
+done
