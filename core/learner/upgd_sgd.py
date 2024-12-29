b73efe9eb974bc10e11dd54b5671a91a1ae1fec2
laschulz <laura@zeus.home>
2024-12-29 13:10:56 +0100

    fixing

--- core/learner/upgd_sgd.py
+++ core/learner/upgd_sgd.py
@@ -1,8 +1,18 @@
 from core.learner.learner import Learner
 from core.optim.gradient_upgd.gradient_upgd import UPGD_SGD
 
+# class UPGD_SGD(Learner):
+#     def __init__(self, network=None, optim_kwargs={}):
+#         optimizer = UPGD_SGD
+#         name = "UPGD_SGD"
+#         super().__init__(name, network, optimizer, optim_kwargs)
+
 class UPGD_SGD(Learner):
     def __init__(self, network=None, optim_kwargs={}):
-        optimizer = UPGD_SGD
-        name = "UPGD_SGD"
-        super().__init__(name, network, optimizer, optim_kwargs)
+        try:
+            print("inside kernel learner")
+            optimizer = UPGD_SGD
+            name = "UPGD_SGD"
+            super().__init__(name, network, optimizer, optim_kwargs)
+        except Exception as e:
+            print(e)
