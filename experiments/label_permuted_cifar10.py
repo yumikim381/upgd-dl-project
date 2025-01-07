5242d112ee345949516867826ef6b13324cf8336
laschulz <laura@Zeus.local>
2025-01-07 22:06:36 +0100

    fixing small error (hopefully)

--- experiments/label_permuted_cifar10.py
+++ experiments/label_permuted_cifar10.py
@@ -1,7 +1,7 @@
 from core.grid_search import GridSearch
 from core.learner.weight_upgd import FirstOrderGlobalUPGDLearner
-from core.learner.sgd import SGDLearner
-from core.learner.shrink_and_perturb import ShrinkandPerturbLearner
+#from core.learner.sgd import SGDLearner
+#from core.learner.shrink_and_perturb import ShrinkandPerturbLearner
 from core.network.fcn_relu import ConvolutionalNetworkReLU
 from core.runner import Runner
 from core.run.run import Run
@@ -51,14 +51,14 @@ grids = [up_grids for _ in range(2)] + [sgd_grid] +  [pgd_grids] + [sp_grid]
 
 learners = [
     FirstOrderGlobalUPGDLearner(),
-    FirstOrderNonprotectingGlobalUPGDLearner(),
-    SGDLearner(),
-    PGDLearner(),
-    ShrinkandPerturbLearner(),
+    # FirstOrderNonprotectingGlobalUPGDLearner(),
+    # SGDLearner(),
+    # PGDLearner(),
+    # ShrinkandPerturbLearner(),
 ]
 
 for learner, grid in zip(learners, grids):
     runner = Runner(Run, learner, grid, exp_name, learner.name)
     runner.write_cmd("generated_cmds")
-    create_script_generator(f"generated_cmds/{exp_name}", exp_name)
-    create_script_runner(f"generated_cmds/{exp_name}")
+    #create_script_generator(f"generated_cmds/{exp_name}", exp_name)
+    #create_script_runner(f"generated_cmds/{exp_name}")
